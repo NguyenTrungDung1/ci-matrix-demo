e86bf518957ae9d1400fd6125745847e34def7c7
NguyenTrungDung1 <dungchicken1234@gmail.com>
2026-04-27 20:59:04 +0700

    "Fix cross-platform path issue"

--- src/test/java/FileTest.java
+++ src/test/java/FileTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -8,8 +9,8 @@ public class FileTest {
 
   @Test
   void testFileExists() {
-    String path = "src\\test\\resources\\data.txt"; // BAD: Windows-only path
+    Path path = Paths.get("src", "test", "resources", "data.txt");
 
-    assertTrue(Files.exists(Paths.get(path)));
+    assertTrue(Files.exists(path));
   }
 }
